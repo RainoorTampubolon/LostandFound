ffe21bc0fc0e0052707f6541c58598e19051c42f
Sinaga541 <57696257+Sinaga541@users.noreply.github.com>
2020-06-16 17:54:44 +0700

    Ivan Felix Sinaga

--- app/src/main/java/com/example/criminalintent/CrimeDetailViewModel.kt
+++ app/src/main/java/com/example/criminalintent/CrimeDetailViewModel.kt
@@ -23,7 +23,7 @@ class CrimeDetailViewModel () : ViewModel(){
 
     fun saveCrime(crime: Crime){
         crimeRepository.updateCrime(crime)
-    }
+    } 
 
     fun getPhotoFile(crime: Crime) : File {
         return crimeRepository.getPhotoFile(crime)
